15a4032f639fbc1932ff4f2c256fd0548cf06371
Lucas Zapico <zapicolucas@gmail.com>
2021-08-20 12:13:13 -0600

    component reducer

---
+++ gatsby-craft-t2/src/components/_index.js
@@ -0,0 +1,14 @@
+// base
+export { default as Nav } from './base/nav/nav';
+
+export { default as Header } from './base/header';
+export { default as Footer } from './base/footer';
+export { default as SEO } from './base/seo';
+export { default as HomeHero } from './base/home-hero';
+export { LinkOne } from './base/buttons-links';
+// elements
+
+// sections
+
+// blocks
+export { default as Slider } from './blocks/slider';
